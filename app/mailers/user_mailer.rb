f8e4f439ceff032f5af815ddbad87c12fe9d9e80
ltevidence <40716120+ltevidence@users.noreply.github.com>
2021-05-17 19:13:07 +0200

    seeding

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'lobotomany@yopmail.com'
+  default from: 'lobotomany@gmail.com'
  
   def welcome_email(user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
