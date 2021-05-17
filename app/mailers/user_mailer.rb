732e30e62d849afd7f4beec30e61d1dff5bddeae
ltevidence <40716120+ltevidence@users.noreply.github.com>
2021-05-17 20:42:20 +0200

    modifying ref

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,14 +12,12 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
 
-  def attendance_email(user)
-    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
-    @user = user 
-
-    #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'http://monsite.fr/attendance' 
-
-    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @user.email, subject: 'Votre participation est validée') 
+  def attendance_email(attendance)
+    @attendance = attendance
+    @attendance_user = User.find(@attendance.user_id)
+    @attendance_event = @attendance.event
+    @admin = User.find(@attendance.event.admin_id)
+    @url = 'monsite.fr' # à utiliser dans la view
+    mail(to: @admin.email, subject: "Un utilisateur à rejoint l'évenement !")
   end
 end
